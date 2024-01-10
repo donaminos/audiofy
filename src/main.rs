d2949ac32a7a207af245f0a86a7ddd1053c80e65
Amine S <amine.s3y@gmail.com>
2024-01-10 08:12:44 +0100

    feat: Read CLI arguments

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+use std::env;
+
 fn main() {
     let msg = "Audiofy: turn your favorite articles into a podcast! 🚀";
     println!("{}", msg); 
+
+    let args: Vec<String> = env::args().collect();
+
+    // args[0] is the path to the program
+    // Further elements are the passed command-line arguments
+    println!("Command-line arguments: {:?}", args);
 }
