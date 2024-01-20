184ecca350d5507dbad568daf409de54827176b9
Amine S <amine.s3y@gmail.com>
2024-01-20 10:43:44 +0100

    feat: instantiate article with fetched data

--- src/article.rs
+++ src/article.rs
@@ -8,12 +8,19 @@ pub struct Article {
 }
 
 impl Article {
-    pub fn new(url: String) -> Article {
-        Article {
-            url: url,
+    async fn new(url: String) -> Result<Article, ValidationError> {
+        let mut article = Article {
+            url,
             title: None,
             content: None,
-        }
+        };
+
+        let fetched_article = fetch_url(&article.url).await?;
+        let (title, content) = parse_html(&fetched_article);
+        article.title = Some(title);
+        article.content = Some(content);
+
+        return Ok(article);
     }
 }
 
@@ -34,19 +41,21 @@ impl Describable for Article {
     }
 }
 
-fn parse_html(html: &str) {
-    // Parse the HTML
+fn parse_html(html: &str) -> (String, String) {
     let document = Html::parse_document(html);
-    // Create a CSS selector
-    let h1_selector = Selector::parse("h1").unwrap();
-    // Get the h1 node content
-    let h1 = document.select(&h1_selector).next().unwrap();
-    let text = h1.text().collect::<Vec<_>>();
 
-    println!("{:?}", text);
+    let h1_selector = Selector::parse("h1").unwrap();
+    let h1 = document.select(&h1_selector).next().unwrap();
+    let h1_content = h1.text().collect::<String>();
+
+    let article_selector = Selector::parse("article").unwrap();
+    let article = document.select(&article_selector).next().unwrap();
+    let article_content = article.text().collect::<String>();
+
+    (h1_content, article_content)
 }
 
-async fn fetch_url(url: &str) {
+async fn fetch_url(url: &str) -> Result<String, ValidationError> {
     println!("Fetching URL {} in progress...", url);
     let response = reqwest::get(url).await;
 
@@ -55,23 +64,19 @@ async fn fetch_url(url: &str) {
             let url_body = res.text().await;
             match url_body {
                 Ok(html) => {
-                    println!("✅ Succeeded!");
-                    parse_html(&html);
+                    println!("✅ Article fetched!");
+                    Ok(html)
                 }
-                Err(e) => {
-                    println!("❌ Failed to get URL body: {}", e.to_string())
-                }
+                Err(_) => Err(ValidationError::UnreachableResource),
             }
         }
-        Err(e) => {
-            println!("❌ Failed to fetch URL: {}", e.to_string());
-        }
+        Err(_) => Err(ValidationError::UnreachableResource),
     }
 }
 
-pub fn get_article(url: String) -> Result<Article, ValidationError> {
+pub async fn get_article(url: String) -> Result<Article, ValidationError> {
     if is_valid_url(&url) {
-        let article = Article::new(url);
+        let article = Article::new(url).await?;
 
         Ok(article)
     } else {
